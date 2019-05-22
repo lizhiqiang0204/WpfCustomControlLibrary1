50d9faed115b4131bbbcaee77a46d12e1fea07a1
lizhiqiang0204 <18518092079@163.com>
2019-05-22 09:53:51 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# WpfCustomControlLibrary1
+# WpfCustomControlLibrary1
---
+++ WpfCustomControlLibrary/CustomControl1.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfCustomControlLibrary
+{
+    /// <summary>
+    /// 按照步骤 1a 或 1b 操作，然后执行步骤 2 以在 XAML 文件中使用此自定义控件。
+    ///
+    /// 步骤 1a) 在当前项目中存在的 XAML 文件中使用该自定义控件。
+    /// 将此 XmlNamespace 特性添加到要使用该特性的标记文件的根
+    /// 元素中:
+    ///
+    ///     xmlns:MyNamespace="clr-namespace:WpfCustomControlLibrary"
+    ///
+    ///
+    /// 步骤 1b) 在其他项目中存在的 XAML 文件中使用该自定义控件。
+    /// 将此 XmlNamespace 特性添加到要使用该特性的标记文件的根
+    /// 元素中:
+    ///
+    ///     xmlns:MyNamespace="clr-namespace:WpfCustomControlLibrary;assembly=WpfCustomControlLibrary"
+    ///
+    /// 您还需要添加一个从 XAML 文件所在的项目到此项目的项目引用，
+    /// 并重新生成以避免编译错误:
+    ///
+    ///     在解决方案资源管理器中右击目标项目，然后依次单击
+    ///     “添加引用”->“项目”->[选择此项目]
+    ///
+    ///
+    /// 步骤 2)
+    /// 继续操作并在 XAML 文件中使用控件。
+    ///
+    ///     <MyNamespace:CustomControl1/>
+    ///
+    /// </summary>
+    public class CustomControl1 : Control
+    {
+        static CustomControl1()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(CustomControl1), new FrameworkPropertyMetadata(typeof(CustomControl1)));
+        }
+    }
+
+    /// <summary>
+    /// BulletCheckBox.xaml 的交互逻辑
+    /// </summary>
+    public class BulletCheckBox : CheckBox
+    {
+        public static readonly DependencyProperty TextProperty = DependencyProperty.Register(
+            "Text", typeof(string), typeof(BulletCheckBox), new PropertyMetadata("Off"));
+        /// <summary>
+        /// 默认文本（未选中）
+        /// </summary>
+        public string Text
+        {
+            get { return (string)GetValue(TextProperty); }
+            set { SetValue(TextProperty, value); }
+        }
+
+        public static readonly DependencyProperty CheckedTextProperty = DependencyProperty.Register(
+            "CheckedText", typeof(string), typeof(BulletCheckBox), new PropertyMetadata("On"));
+        /// <summary>
+        /// 选中状态文本
+        /// </summary>
+        public string CheckedText
+        {
+            get { return (string)GetValue(CheckedTextProperty); }
+            set { SetValue(CheckedTextProperty, value); }
+        }
+
+        public static readonly DependencyProperty CheckedForegroundProperty =
+            DependencyProperty.Register("CheckedForeground", typeof(Brush), typeof(BulletCheckBox), new PropertyMetadata(Brushes.WhiteSmoke));
+        /// <summary>
+        /// 选中状态前景样式
+        /// </summary>
+        public Brush CheckedForeground
+        {
+            get { return (Brush)GetValue(CheckedForegroundProperty); }
+            set { SetValue(CheckedForegroundProperty, value); }
+        }
+
+        public static readonly DependencyProperty CheckedBackgroundProperty =
+            DependencyProperty.Register("CheckedBackground", typeof(Brush), typeof(BulletCheckBox), new PropertyMetadata(Brushes.LimeGreen));
+        /// <summary>
+        /// 选中状态背景色
+        /// </summary>
+        public Brush CheckedBackground
+        {
+            get { return (Brush)GetValue(CheckedBackgroundProperty); }
+            set { SetValue(CheckedBackgroundProperty, value); }
+        }
+
+        static BulletCheckBox()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(BulletCheckBox), new FrameworkPropertyMetadata(typeof(BulletCheckBox)));
+        }
+    }
+}
